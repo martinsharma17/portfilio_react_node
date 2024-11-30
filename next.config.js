8d5c34d9f127acd0a418bb7cd3a6352fef13fe5a
martinsharma17 <martinsharma18@gmail.com>
2024-11-30 20:58:36 +0545

    .

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const path = require('path')
- 
+
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
@@ -8,16 +8,17 @@ module.exports = {
     domains: ['res.cloudinary.com', 'media.dev.to']
   }
 }
-
 const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Disable default image optimization
+    unoptimized: true, // For static exports
+    domains: ['media2.dev.to'], // Add external domains here
   },
-  assetPrefix: isProd ? '/portfolio_react_node/' : '', // Use your repository name
+  assetPrefix: isProd ? '/portfolio_react_node/' : '',
   basePath: isProd ? '/portfolio_react_node' : '',
-  output: 'export'
+  output: 'export',
 };
 
-export default nextConfig;
+module.exports = nextConfig; // Or export default if using ES modules
