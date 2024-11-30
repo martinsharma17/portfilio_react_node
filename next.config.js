53b8a499591c83f76545e02f5d7ab8d1d57b5f88
martinsharma17 <martinsharma18@gmail.com>
2024-11-30 20:22:44 +0545

    Initial commit

--- next.config.js
+++ next.config.js
@@ -8,3 +8,16 @@ module.exports = {
     domains: ['res.cloudinary.com', 'media.dev.to']
   }
 }
+
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/portfolio_react_node/' : '', // Use your repository name
+  basePath: isProd ? '/portfolio_react_node' : '',
+  output: 'export'
+};
+
+export default nextConfig;
